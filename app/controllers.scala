bbe122db8781a66f76b5954edfa42a6e6c624d85
Jieren Chen <jieren.chen@gmail.com>
2012-02-27 15:45:34 -0500

    many adjustments to get authentication working

--- app/controllers.scala
+++ app/controllers.scala
@@ -9,24 +9,25 @@ object Application extends Controller {
     
     import views.Application._
     
-    private val mongoUri = new MongoURI(new com.mongodb.MongoURI(Properties.envOrElse("MONGOLAB_URI", "mongodb://127.0.0.1:27017/test")))
-    val _mongoConn = MongoConnection(mongoUri)
+    private val mongoUri = new MongoURI(new com.mongodb.MongoURI(Properties.envOrElse("MONGOLAB_URI", "mongodb://127.0.0.1:27017/heroku_app3056061")))
+    //val _mongoConn = MongoConnection(new String(mongoUri.password),mongoUri.hosts(0).toInt)
+    val _mongoConn = MongoConnection("ds031087.mongolab.com",31087)
+  
+    val credsSplit = mongoUri.username.split(":")
+    val username = credsSplit(0)
+    val password = credsSplit(1)
+    if (password != null)
+      _mongoConn("heroku_app3056061").authenticate(username,password)
+    val database = _mongoConn("heroku_app3056061")("dubstep")
     
     def index = {
-	Text(mongoUri)
-	/*
-	if (mongoUri.password == null || _mongoConn("test").authenticate(mongoUri.username,mongoUri.password.asInstanceOf[String])){
-	  val database = _mongoConn("test")("dubstep")
-	  val count : Int = database.count.asInstanceOf[Int]
-	  val randGen = new scala.util.Random
-	  val rand = randGen.nextInt(count)
-	  val msgs = database.find("msg" $exists true $ne "").limit(1).skip(rand)
-	  val msgStrings = msgs.map ( (obj : DBObject ) => obj.getOrElse("msg","") )
-	  html.index("What does dubstep sound like?",msgStrings.toSeq)	  
-	}
-	else {
-	  html.index("What does dubstep sound like?",List("404 errors"))
-	}*/
+      //Text(Properties.envOrElse("MONGOLAB_URI", "mongodb://127.0.0.1:27017/test") + "\n" + mongoUri.username + "\n" + mongoUri.hosts(0).toInt + "\n" + new String(mongoUri.password))
+      val count : Int = database.count.asInstanceOf[Int]
+      val randGen = new scala.util.Random
+      val rand = if (count > 0) randGen.nextInt(count); else 0;
+      val msgs = database.find("msg" $exists true $ne "").limit(1).skip(rand)
+      val msgStrings = msgs.map ( (obj : DBObject ) => obj.getOrElse("msg","") )
+      html.index("What does dubstep sound like?",msgStrings.toSeq)
     }
     
     def submitGet = {
@@ -35,7 +36,7 @@ object Application extends Controller {
     
     def submitPost (msg : String) = {
 	val doc = MongoDBObject("msg" -> msg, "random" -> Math.random)
-	_mongoConn("test")("dubstep").save( doc )
+	database.save( doc )
 	Redirect("/")
     }
     
